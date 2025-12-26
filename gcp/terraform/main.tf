eb7a22d638e4229898f579c8c2abb4cafcb92095
Dan Phillips <dan@danphillips.info>
2025-12-26 13:14:33 -0500

    removed expensive CDN. going with CloudFlare

--- gcp/terraform/main.tf
+++ gcp/terraform/main.tf
@@ -18,12 +18,12 @@ resource "google_storage_bucket" "static_site" {
 
   # CORS configuration for future API calls (visitor counter)
   cors {
-    origin          = ["http://${var.www_domain_name}"]
+    origin          = ["https://${var.www_domain_name}"]
     method          = ["GET", "HEAD", "POST"]
     response_header = ["Content-Type"]
     max_age_seconds = 3600
   }
 }
 
-# Note: Public access will be set via object-level ACLs after upload
-# Organization policy blocks bucket-level allUsers access
+# Note: Public access will be handled via Cloudflare
+# Bucket can remain private - Cloudflare will proxy requests
--- gcp/terraform/outputs.tf
+++ gcp/terraform/outputs.tf
@@ -4,26 +4,11 @@ output "bucket_name" {
 }
 
 output "bucket_url" {
-  description = "Public URL to access the site"
-  value       = "http://storage.googleapis.com/${google_storage_bucket.static_site.name}/index.html"
+  description = "Direct GCS bucket URL"
+  value       = "https://storage.googleapis.com/${google_storage_bucket.static_site.name}/index.html"
 }
 
-output "cname_target" {
-  description = "CNAME target for DNS configuration"
-  value       = "c.storage.googleapis.com"
-}
-
-output "dns_instructions" {
-  description = "DNS configuration instructions"
-  value       = <<-EOT
-    Configure your DNS with this CNAME record:
-    
-    Name: www
-    Type: CNAME
-    Value: c.storage.googleapis.com
-    TTL: 300
-    
-    After DNS propagates, access your site at:
-    http://www.danphillips.cloud
-  EOT
+output "cloudflare_origin" {
+  description = "Origin URL for Cloudflare configuration"
+  value       = "storage.googleapis.com/${google_storage_bucket.static_site.name}"
 }
