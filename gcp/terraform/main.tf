41772faa2440b61fd5234df188607ef4506ddb54
Dan Phillips <dan@danphillips.info>
2025-12-26 16:08:33 -0500

    working CloudFlare deploy with GCP. Migrated state to TF Cloud.

--- gcp/terraform/main.tf
+++ gcp/terraform/main.tf
@@ -1,6 +1,19 @@
+# Provider configuration
 provider "google" {
-  project = var.project_id
-  region  = var.region
+  credentials = var.gcp_credentials
+  project     = var.project_id
+  region      = var.region
+}
+
+# Storing our state in HCP Cloud
+terraform { 
+  cloud { 
+    organization = "SluggerField" 
+
+    workspaces { 
+      name = "gcp-danphillips-cloud" 
+    } 
+  } 
 }
 
 # Storage bucket for static site
@@ -18,12 +31,16 @@ resource "google_storage_bucket" "static_site" {
 
   # CORS configuration for future API calls (visitor counter)
   cors {
-    origin          = ["https://${var.www_domain_name}"]
+    origin          = ["https://danphillips.cloud", "https://www.danphillips.cloud"]
     method          = ["GET", "HEAD", "POST"]
     response_header = ["Content-Type"]
     max_age_seconds = 3600
   }
 }
 
-# Note: Public access will be handled via Cloudflare
-# Bucket can remain private - Cloudflare will proxy requests
+# Make bucket publicly readable
+resource "google_storage_bucket_iam_member" "public_read" {
+  bucket = google_storage_bucket.static_site.name
+  role   = "roles/storage.objectViewer"
+  member = "allUsers"
+}
