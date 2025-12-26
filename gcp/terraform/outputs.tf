651bc9c7d7e29a36f875c30db0dd47b87f126858
Dan Phillips <dan@danphillips.info>
2025-12-26 12:29:34 -0500

    files deployed to CGP!

--- gcp/terraform/outputs.tf
+++ gcp/terraform/outputs.tf
@@ -5,10 +5,25 @@ output "bucket_name" {
 
 output "bucket_url" {
   description = "Public URL to access the site"
-  value       = "https://storage.googleapis.com/${google_storage_bucket.static_site.name}/index.html"
+  value       = "http://storage.googleapis.com/${google_storage_bucket.static_site.name}/index.html"
 }
 
-output "website_endpoint" {
-  description = "Direct bucket website endpoint"
-  value       = google_storage_bucket.static_site.url
+output "cname_target" {
+  description = "CNAME target for DNS configuration"
+  value       = "c.storage.googleapis.com"
+}
+
+output "dns_instructions" {
+  description = "DNS configuration instructions"
+  value       = <<-EOT
+    Configure your DNS with this CNAME record:
+    
+    Name: www
+    Type: CNAME
+    Value: c.storage.googleapis.com
+    TTL: 300
+    
+    After DNS propagates, access your site at:
+    http://www.danphillips.cloud
+  EOT
 }
