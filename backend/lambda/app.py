8a33dc8539daa2f3671832a621e1ffd384a892b0
Dan Phillips <dan@danphillips.info>
2025-12-27 14:13:01 -0500

    SAM and Labmda code complete. time to test the counter

---
+++ backend/lambda/app.py
@@ -0,0 +1,46 @@
+import json
+import boto3
+import os
+
+dynamodb = boto3.resource('dynamodb')
+table_name = os.environ['TABLE_NAME']
+table = dynamodb.Table(table_name)
+cors_origin = os.environ.get('CORS_ORIGIN', '*')
+
+COUNTER_ID = 'website-visits'
+
+def lambda_handler(event, context):
+    try:
+        response = table.update_item(
+            Key={'id': COUNTER_ID},
+            UpdateExpression='SET visit_count = if_not_exists(visit_count, :start) + :inc',
+            ExpressionAttributeValues={
+                ':inc': 1,
+                ':start': 0
+            },
+            ReturnValues='UPDATED_NEW'
+        )
+        
+        visit_count = int(response['Attributes']['visit_count'])
+        
+        return {
+            'statusCode': 200,
+            'headers': {
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': cors_origin,
+                'Access-Control-Allow-Headers': 'Content-Type',
+                'Access-Control-Allow-Methods': 'GET, OPTIONS'
+            },
+            'body': json.dumps({'count': visit_count})
+        }
+        
+    except Exception as e:
+        print(f"Error: {str(e)}")
+        return {
+            'statusCode': 500,
+            'headers': {
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': cors_origin
+            },
+            'body': json.dumps({'error': 'Failed to update visitor count'})
+        }
